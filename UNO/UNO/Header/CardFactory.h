453434360e3b0110cda074bb316fcb8cdbdd31e8
Trombetta <trombetta.mateus@gmail.com>
2024-01-15 17:39:38 -0300

    Created Card factory and removed CardUtils

---
+++ UNO/UNO/Header/CardFactory.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include <memory>
+
+enum class card_color;
+class Card;
+
+class CardFactory
+{
+public:
+    [[nodiscard]] std::unique_ptr<Card> CreateWildCard() const;
+    [[nodiscard]] std::unique_ptr<Card> CreateNumberCard(card_color color, int number) const;
+    [[nodiscard]] std::unique_ptr<Card> CreateReverseCard(card_color color) const;
+    [[nodiscard]] std::unique_ptr<Card> CreatePlusCard(card_color color, int cards_to_draw) const;
+    [[nodiscard]] std::unique_ptr<Card> CreateSkipCard(card_color color) const;
+};
--- UNO/UNO/Header/CardUtils.cpp
+++
@@ -1,1 +0,0 @@
-﻿#include "CardUtils.h"
